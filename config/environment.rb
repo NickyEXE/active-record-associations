bfd5cf3a08a5764cefb4f5b5bb0218f21354fa0e
NickyEXE <dovern42@gmail.com>
2021-04-15 17:42:55 -0400

    move the files around

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,9 @@ require 'bundler'
 Bundler.require
 Dotenv.load
 
-require_relative "../models/cli.rb"
-require_relative "../models/breaking_bad_api.rb"
-require_relative "../models/character.rb"
-require_relative "../models/message.rb"
-require_relative "../models/user.rb"
-require_relative "./seeds.rb"
+require_relative "../app/models/cli.rb"
+require_relative "../app/models/breaking_bad_api.rb"
+require_relative "../app/models/character.rb"
+require_relative "../app/models/message.rb"
+require_relative "../app/models/user.rb"
+require_relative "../db/seeds.rb"
